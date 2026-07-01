d91af1025d9251644554849b09c44503c00acaed
Droom-Sikandar <sikandar.vishwakarma@droom.in>
2026-07-01 17:46:39 +0530

    Last Update code with context

--- src/main/java/com/example/springdemo/SpringDemoApplication.java
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -9,6 +9,11 @@ public class SpringDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 
+		/*ApplicationContext ctx=SpringApplication.run(SpringDemoApplication.class, args);
+		ApisController api=ctx.getBean(ApisController.class);
+         String str=api.hello();
+		System.out.println(str);*/
+
 		/*ApisController apiController=new ApisController();
 		String str=apiController.hello();
 		System.out.println(str);*/
